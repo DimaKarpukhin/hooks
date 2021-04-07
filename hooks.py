24276b63d3468a8644c243f731ca838b54d70b1f
DimaKarpukhin <dimadk787@gmaile.com>
2021-04-07 16:46:25 +0300

    Added hooks.py file

---
+++ hooks.py
@@ -0,0 +1,13 @@
+def say_hello(name):
+    print(f'Hello {name} =)')
+
+
+def factorial(num):
+    for x in range(1, num):
+        res = x * (x+1)
+    return res
+
+
+if __name__ == "__main__":
+    say_hello('Dima')
+    print(factorial(5))
