99e316d31570cef8ec2c9cad02b0faeeb97323f9
DimaKarpukhin <dimadk787@gmaile.com>
2021-04-11 01:10:40 +0300

    Testing pre-commit

--- pre-push.py
+++ pre-push.py
@@ -14,22 +14,22 @@ Installing pre-push Flake8 hook throug command line(copy & paste):
 
 def main():
     flake8_path = '/usr/local/bin/flake8'
-    result = subprocess.run([flake8_path], stdout=subprocess.PIPE)
-    is_failed = result.stdout.decode('utf-8')
-    if is_failed:
-        print(color.BLUE + '\nFlake8'
-              + color.BLUE + ' ------------------------> '
-              + color.FAILED + 'Failed!')
-        print(
-            f'{color.RED}Please correct following linting errors:')
-        print(f'{color.FAILED}{result.stdout.decode("utf-8")}{color.RESET}')
-        sys.exit(1)
-    else:
-        print(color.BLUE + '\nFlake8'
-              + color.BLUE + ' ------------------------> '
-              + color.PASSED + 'Passed!')
-        print(f"{color.CYAN}'git push' succeeded!\n{color.RESET}")
-        sys.exit(0)
+    # result = subprocess.run([flake8_path], stdout=subprocess.PIPE)
+    # is_failed = result.stdout.decode('utf-8')
+    # if is_failed:
+    #     print(color.BLUE + '\nFlake8'
+    #           + color.BLUE + ' ------------------------> '
+    #           + color.FAILED + 'Failed!')
+    #     print(
+    #         f'{color.RED}Please correct following linting errors:')
+    #     print(f'{color.FAILED}{result.stdout.decode("utf-8")}{color.RESET}')
+    #     sys.exit(1)
+    # else:
+    #     print(color.BLUE + '\nFlake8'
+    #           + color.BLUE + ' ------------------------> '
+    #           + color.PASSED + 'Passed!')
+    #     print(f"{color.CYAN}'git push' succeeded!\n{color.RESET}")
+    #     sys.exit(0)
 
 
 if __name__ == "__main__":
