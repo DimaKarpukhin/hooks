81a1c87189c69693b1c2ca2e3be716a4c4f49dda
DimaKarpukhin <dimadk787@gmaile.com>
2021-04-21 13:06:32 +0300

    Test

--- pre-push.py
+++ pre-push.py
@@ -13,7 +13,7 @@ Installing pre-push Flake8 hook throug command line(copy & paste:
 
 
 def main():
-    flake8_path = '/usr/local/bin/flake8'
+    flake8_path = '/usr/local/bin/flake8  '
     result = subprocess.run([flake8_path], stdout=subprocess.PIPE)
     is_failed = result.stdout.decode('utf-8')
     if is_failed:
