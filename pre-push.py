f050a9007185a3a2682dc5fa58688beb9e588ebc
DimaKarpukhin <dimadk787@gmaile.com>
2021-04-21 11:13:02 +0300

    Add a in b from c\n\n - Fix a

--- pre-push.py
+++ pre-push.py
@@ -5,7 +5,7 @@ import sys
 import color
 
 """
-Installing pre-push Flake8 hook throug command line(copy & paste)  :
+Installing pre-push Flake8 hook throug command line(copy & paste):
  1. git clone https://github.com/DimaKarpukhin/pre-push-hook.git
  2. cd pre-push-hook && cat /dev/null > .git/hooks/pre-push
  3. ln -s -f ../../pre-push.py .git/hooks/pre-push && chmod +x pre-push.py
